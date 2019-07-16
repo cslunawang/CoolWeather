abca1d6d33db5264ad07683b623253bc14b71864
wly <847353428@qq.com>
2019-07-16 10:27:48 +0800

    F

---
+++ app/src/main/java/com/example/coolweather/db/Country.java
@@ -0,0 +1,12 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+
+}
